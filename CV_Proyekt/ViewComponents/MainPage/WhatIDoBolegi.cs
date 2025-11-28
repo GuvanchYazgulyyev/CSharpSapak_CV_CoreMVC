858680276702491b619bc46dba64751134626679
Guvanch <mr.yazgulyyew@outlook.com>
2025-11-28 22:56:56 +0300

    What I Do goşuldy!!

---
+++ CV_Proyekt/ViewComponents/MainPage/WhatIDoBolegi.cs
@@ -0,0 +1,15 @@
+﻿using CV_Proyekt.Models.SqlModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Proyekt.ViewComponents.MainPage
+{
+    public class WhatIDoBolegi : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var getData = context.WhatIdos.ToList();
+            return View(getData);
+        }
+    }
+}
