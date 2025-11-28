868c614af466bcc04f29d4068a4665d8205d6528
Yusup <Yusup@DESKTOP-6CBBCTI>
2025-11-28 21:34:10 +0200

    New Versıon

--- CV_Proyekt/Controllers/VitrinController.cs
+++ CV_Proyekt/Controllers/VitrinController.cs
@@ -18,6 +18,7 @@ namespace CV_Proyekt.Controllers
             var dataGetir = await db.AboutMes.OrderByDescending(f => f.Id).AsNoTracking().ToListAsync();
             return View(dataGetir);
         }
+
        /// !!! TASK  Geljek sapaka  TAze Controller. Açmaly. Bu Controller Admin Tarapy üçindir!  21-11-2025
        /// We şol bir wagtda  Admin üçin Layout açmaly. 
        /// Controller için  CRUD işlemleri bolmaly. Controller içinde Dine Şuwagtlykça IActionRessultlar YAzylymaly.
